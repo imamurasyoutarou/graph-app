d1710f69b8b999de23004a83efccf9f41f575cf9
syoutarou imamura <syoutarouimamura@gmail.com>
2022-07-16 21:38:00 +0900

    Graphのstyle変更

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -7,16 +7,19 @@ type Props = {
 }
 
 export const Graph: React.FC<Props> = ({ data, populations }) => {
+  const width = window.innerWidth - 40
   return (
-    <LineChart width={400} height={400} data={data}>
+    <LineChart width={width} height={500} data={data}>
       {populations.map(({ prefName, color }: { prefName: string; color: string }) => {
         return <Line type='monotone' key={prefName} dataKey={prefName} stroke={color} />
       })}
       <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
-      <XAxis dataKey='年度'>
-        <Label value='年度' position='insideBottomRight' />
+      <XAxis padding={{ right: 64 }} dataKey='年度' fontSize={14}>
+        <Label fontWeight='bold' fontSize={14} value='年度' position='insideBottomRight' />
       </XAxis>
-      <YAxis label={{ value: '人口数', angle: 0, position: 'insideTopLeft' }} />
+      <YAxis tickCount={8} fontSize={14} padding={{ top: 64 }}>
+        <Label fontSize={14} value='人口数' fontWeight='bold' position='insideTopLeft' />
+      </YAxis>
       <Legend verticalAlign='top' />
       <Tooltip />
     </LineChart>

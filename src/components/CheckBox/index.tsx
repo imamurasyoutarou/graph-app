a871c6495af37a5ed581c906f577efacebc3f095
syoutarou imamura <syoutarouimamura@gmail.com>
2022-07-16 19:20:31 +0900

    都道府県をmapで表示

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 type Props = {
   prefName: string
-  onChange: () => void
+  prefCode: number
+  onChange: (prefCode: number, prefName: string, checked: boolean) => void
 }
 
-export const CheckBox: React.FC<Props> = ({ prefName, onChange }) => {
+export const CheckBox: React.FC<Props> = ({ prefName, prefCode, onChange }) => {
+  const getGraph = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(prefCode, prefName, e.target.checked)
+  }
   return (
     <label>
-      <input onChange={onChange} type='checkbox' />
+      <input onChange={(e) => getGraph(e)} type='checkbox' />
       {prefName}
     </label>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
 import type { NextPage } from 'next'
+import { CheckBox } from '@/components/CheckBox'
 import { Layout } from '@/components/Layout'
 import { getPrefecturesData } from '@/lib/resas-api'
 
-const Home: NextPage = ({ prefectures }) => {
-  return <Layout>都道府県</Layout>
+type Props = {
+  prefectures: any
+}
+const Home: NextPage<Props> = ({ prefectures }) => {
+  const onChengeGraph = (prefCode: number, prefName: string, checked: boolean) => {
+    console.log(prefCode, prefName, checked)
+  }
+  return (
+    <Layout>
+      都道府県
+      {prefectures && (
+        <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {prefectures.map(({ prefName, prefCode }: { prefName: string; prefCode: number }) => {
+            return (
+              <li key={prefName} style={{ margin: 10 }}>
+                <CheckBox onChange={onChengeGraph} prefName={prefName} prefCode={prefCode} />
+              </li>
+            )
+          })}
+        </ul>
+      )}
+    </Layout>
+  )
 }
 
 export default Home

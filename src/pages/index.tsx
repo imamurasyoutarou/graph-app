52a0c7fcf4d02bf3a3fc0eaef87d6b02eace5670
syoutarou imamura <syoutarouimamura@gmail.com>
2022-07-16 19:00:28 +0900

    RESAS API導入 &取得関数作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next'
 import { Layout } from '@/components/Layout'
+import { getPrefecturesData } from '@/lib/resas-api'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ prefectures }) => {
   return <Layout>都道府県</Layout>
 }
 
 export default Home
+
+export const getStaticProps = async () => {
+  const prefectures = await getPrefecturesData()
+
+  return {
+    props: { prefectures },
+  }
+}
